626d9f918013f4b8ee684e5308a1dedf477c97d1
Sou <samelsonjohn@protonmail.com>
2023-08-21 22:24:40 -0400

    edit Top Charts style with text

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -17,6 +17,11 @@ const TopChartCard = ({ song, i }) => (
     <h3 className="fot-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
       <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+      </div>
     </div>
   </div>
 );
